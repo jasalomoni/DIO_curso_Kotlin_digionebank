8736b57e18edc06d5c504b4d4d1ccffbff282e22
José Augusto Salomoni <zeca.salomoni@gmail.com>
2021-02-19 10:26:51 -0300

    alterando classes Funcionario e Pessoa para abstract

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -3,10 +3,10 @@ package one.digitalinnovation.digionebank
 import one.digitalinnovation.digionebank.Pessoa
 import java.math.BigDecimal
 
-class Funcionario(
-    override val nome: String,
-    override val cpf: String,
+abstract class Funcionario(
+    nome: String,
+    cpf: String,
     val salario: BigDecimal
 ) : Pessoa(nome, cpf) {
-
+    abstract fun calculoBonus()
 }
